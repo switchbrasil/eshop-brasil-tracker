941fa419902e78901b426644c7d0379a93866271
stephannv <3025661+stephannv@users.noreply.github.com>
2020-12-19 18:57:39 -0300

    Improve discord notifications

--- app/models/price.rb
+++ app/models/price.rb
@@ -27,38 +27,53 @@ class Price < ApplicationRecord
   end
 
   def create_discord_notification
-    if (saved_change_to_id && saved_change_to_id.first.nil?) || (current_price_changed? && has_price_drop?)
-      discord_notification = discord_notifications.new(
-        title: item.title,
-        url: item.website_url,
-        thumbnail: item.main_picture_url,
-        fields: [{ name: 'Preço Atual', value: current_price.format }]
-      )
-      if discount_price.present?
-        description = saved_change_to_id? ? '🔥🤑 Preço revelado e com desconto!!!' : '🤑 Jogo em promoção!!!'
-        discord_notification.description = description
+    return unless current_price_changed?
+
+    discord_notification = discord_notifications.new(discord_notification_attributes)
+
+    if saved_change_to_discount_price_cents?
+      if discount_price.nil?
+        discord_notification.description = '😭 Promoção encerrada.'
+      else
         discord_notification.fields += [
           { name: 'Preço sem desconto', value: regular_price.format },
           { name: 'Desconto', value: "#{discount_percentage}% OFF" },
           { name: 'Inicia em', value: I18n.l(discount_start_date), inline: true },
           { name: 'Vai até', value: I18n.l(discount_end_date), inline: true }
         ]
-      else
-        discord_notification.description = '🔥 Preço revelado!!!'
+        if saved_change_to_id?
+          discord_notification.description = '🔥🤑 Preço revelado e com desconto!!!'
+        else
+          discord_notification.description = '🤑 Jogo em promoção!!!'
+        end
       end
-      discord_notification.save!
+    elsif saved_change_to_regular_price_cents?
+      if saved_change_to_id?
+        discord_notification.description = '🔥 Preço revelado!!!'
+      else
+        discord_notification.description = '🔧 Reajuste de preço'
+        old_price = Money.new(saved_change_to_regular_price_cents.first.to_i, current_price.currency.iso_code)
+        discord_notification.fields += [
+          { name: 'Preço antes do reajuste', value: old_price.format }
+        ]
+      end
     end
+
+    discord_notification.save!
   end
 
   def current_price_changed?
     saved_change_to_regular_price_cents? || saved_change_to_discount_price_cents?
   end
 
-  def has_price_drop?
-    if saved_change_to_discount_price_cents?
-      saved_change_to_discount_price_cents.first.to_i == 0 || saved_change_to_discount_price_cents.second.to_i < saved_change_to_discount_price_cents.first.to_i
-    elsif saved_change_to_regular_price_cents?
-      saved_change_to_regular_price_cents.second.to_i < saved_change_to_regular_price_cents.first.to_i
-    end
+  def discord_notification_attributes
+    {
+      title: item.title,
+      url: item.website_url,
+      thumbnail: item.main_picture_url,
+      fields: [
+        { name: 'Preço Atual', value: current_price.format }
+      ]
+    }
   end
 end
